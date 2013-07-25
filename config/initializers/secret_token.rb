4b918f43b6ec8460e0d240401556f3017a167ea4
Snehal Sakre <sne.ringane@gmail.com>
2013-07-25 16:34:13 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekhours::Application.config.secret_token = 'a1aba2301234ee4f1a02176d615a94810cf7cf6cb5dee1560cef9cec08c9f90412d91bc69c95603daf99c6cc05c8bb01f423b4db79bb605796de6961978e7523'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Geekhours::Application.config.session_store :cookie_store, key: '_geekhours_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Geekhours::Application.config.session_store :active_record_store
