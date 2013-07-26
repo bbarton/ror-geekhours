057d08e50cb7cfcb9a59fa475976512056793a9e
Snehal Sakre <sne.ringane@gmail.com>
2013-07-26 16:41:31 +0530

    few changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_many :secondary_users, :dependent => :destroy
 
   accepts_nested_attributes_for :secondary_users
-  attr_accessible :email, :name, :team_name,:secondary_users_attributes
+  attr_accessible :email, :name, :team_name,:description,:secondary_users_attributes
 
 end
