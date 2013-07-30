c16eb70291b547597c00adc46ab60715132e97f0
Vivek Parihar <vivek@weboniselab.com>
2013-07-30 15:01:07 +0530

    mailers changed

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def send_invite (email)
-  mail(:to =>email,:from=>"contest@webonise.com",:content_type=> 'text/html')
+  mail(:to =>email,:from=>"vivek@webonise.com",:content_type=> 'text/html')
   end
 end
