fbebdf90373bf5fa9c2a1f01433e6af980e70738
vivek parihar <vivek@weboniselab.com>
2013-07-30 07:06:00 +0530

    Delete sendgrid_credentials.rb

--- config/initializers/sendgrid_credentials.rb
+++
@@ -1,9 +0,0 @@
-#delivery_method :smtp, { :address              => "smtp.sendgrid.net",
-#                         :port                 => 587,
-#                         :domain               => 'webonise.com',
-#                         :user_name            => 'bhuvan',
-#                         :password             => 'webonise6186',
-#                         :authentication       => 'plain',
-#                         :enable_starttls_auto => true }
-#
-##
