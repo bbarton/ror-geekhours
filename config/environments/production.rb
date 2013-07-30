6c67ae8f0a4e8e7c97ef01e74e3e44396624ec15
Vivek Parihar <vivek@weboniselab.com>
2013-07-30 23:25:57 +0530

    added migration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Geekhours::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( jquery.min.js )
+  config.assets.precompile += %w( admin.css admin.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
