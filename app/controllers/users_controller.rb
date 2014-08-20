e8e1e669983702abec3710d3b46f2fa11783fa62
deepti-webonise <deepti@weboniselab.com>
2014-08-20 16:46:43 +0530

    secondary users are successfully saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,23 +14,18 @@ class UsersController < ApplicationController
         format.html {  redirect_to root_path }
         format.js
       else
-        @user = User.new(params[:user])
-        if @user.save
-          User.last.update_attributes(:name => User.last.name.capitalize)
-          UserMailer.delay.welcome_email(User.last)
-          #if @user.secondary_users.present?
-          #  @user.secondary_users.each do |sec_user|
-          #    UserMailer.delay.welcome_secondary_email(sec_user,@user.name)
-          #  end
-          #end
-          flash[:notice] = nil
-          format.html { redirect_to registered_user_users_path}
-          format.js
-        else
-          flash[:notice] = "Email Already Exists"
-          format.html {  redirect_to root_path }
-          format.js
-        end
+         @user = User.new(params[:user])
+         if @user.save
+           User.last.update_attributes(:name => User.last.name.capitalize)
+           UserMailer.delay.welcome_email(User.last)
+           flash[:notice] = nil
+           format.html { redirect_to registered_user_users_path}
+           format.js
+         else
+           flash[:notice] = "Email Already Exists"
+           format.html {  redirect_to root_path }
+           format.js
+         end
 
       end
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,13 +20,13 @@ Geekhours::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost://3000' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => 'mail.weboniselab.com',
-      :user_name            => 'pansingh@weboniselab.com',
-      :password             => 'pansingh6186',
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
+      :enable_starttls_auto => true,
+      :address            => "smtp.gmail.com",
+      :port               => "587",
+      :domain             => "gmail.com",
+      :authentication     => :plain,
+      :user_name          => "pansingh@weboniselab.com",
+      :password           => "pansingh6186"
   }
   config.action_mailer.asset_host = 'http://localhost:3000'
   UserMailer.smtp_settings = Settings.user_mailer
