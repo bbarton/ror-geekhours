e4437f6bc08b65b3ca441c1a5e47ea293f633344
Swapnil Vathare <swapnil@weboniselab.com>
2015-10-08 21:14:55 +0530

    git 2015 complete

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,5 +1,5 @@
 class AdminMailer < ActionMailer::Base
-  default from: "vivek@webonise.com"
+  default from: "kvijay@weboniselab.com"
 
   def send_invite (email)
     mail(:to =>email, subject: "Webonise Lab : Geek Hours 2015!")
@@ -8,6 +8,6 @@ class AdminMailer < ActionMailer::Base
   def send_hall_ticket(user, secondary_users)
     @user = user
     @secondary_users = secondary_users
-    mail(:to => user.email, subject: "Johnnie Coder: Hall Ticket of Geek Hours 2015!")
+    mail(:to => user.email, subject: "Super Coder: Hall Ticket of Geek Hours 2015!")
   end
 end
