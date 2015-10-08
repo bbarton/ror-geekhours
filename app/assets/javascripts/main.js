a7c2f4e47d7d0c0a09c1ab6b5f2dbb136c02ab4c
Swapnil Vathare <swapnil@weboniselab.com>
2015-10-08 20:29:44 +0530

    2015 added

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,81 +1,134 @@
-$(document).ready(function(){
-var windowHeight = $(window).height();
-var windowWidth= $(window).width();
+		$(document).ready(function() {
 
-// Background image animation
-var bgImg = $('.mainBg');
-var bgImgHeight = windowHeight;
-var bgImgWidth = (bgImgHeight/800)*780;
-var bgImgLeft = (windowWidth-bgImgWidth)/2;
-var bgImgLeftAnimate = bgImgLeft-(bgImgWidth/2.5);
+			/*
+			* Plugin intialization
+			*/
+	    	$('#pagepiling').pagepiling({
+	    		/*//menu: '#menu',
+	    		anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+			    //sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C'],
+			    navigation: {
+			    	//'position': 'right',
+			   		'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5']
+			   	},*/
+			    /*afterRender: function(){
+			    	$('#pp-nav').addClass('custom');
+			    },*/
+			    afterLoad: function(anchorLink, index){
+			    	if(index==5){
+			    		$('.downArrow').removeClass('active');
+			    		$('.downArrow').addClass('hideArrow');
+			    	}else{
+			    		$('.downArrow').addClass('active');
+			    		$('.downArrow').removeClass('hideArrow');
+			    	}
+			    }
+			});
+			$('.downArrow').click(function(e){
+				e.stopPropagation();
+				e.preventDefault();
+		    	$.fn.pagepiling.moveSectionDown();
+			});
+	    	var windowWidth = $(window).width();
+	    	var wrapperWidht = windowWidth-385;
+	    	var registerWrap = $('.registerWrap');
+            var galleryWrap = $(".galleryWrap");
+	    	registerWrap.css({
+	    		width: wrapperWidht,
+	    		right: -wrapperWidht
+	    	});
+            galleryWrap.css({
+                width: wrapperWidht,
+                right: -wrapperWidht
+            });
 
-bgImg.css({
-    height: bgImgHeight,
-    left:bgImgLeft
-});
-//bgImg.load(function(){
-    $('.loader').fadeOut(500);
-    setTimeout(function(){
-        bgImg.animate({
-            left:bgImgLeftAnimate
-        },1500,function(){
-//            $('.homeInner').fadeIn(1000);
-            $('.homeInner, .galleryInner, .registerForm').animate({
-                opacity:1
-            },1000);
+
+			var gh = $( ".ghLogo" );
+			var ghl;
+			//var ght;
+	    	$('.linkButton').click(function(e) {
+				e.stopPropagation();
+				e.preventDefault();
+                var whichWrap = $(this).data("target");
+				var offset = gh.offset();
+				ghl = offset.left;
+				//ght = offset.top;
+
+				gh.css({
+					position: 'relative'
+				})
+	    		/* Act on the event */
+                $(whichWrap).animate({
+		    		right: 0
+		    	},1000);
+                $(whichWrap).addClass("activeSide");
+		    	gh.animate({
+		    		left: -(ghl - 115)},
+		    		1000);
+		    	/*$('.codersLogo').animate({
+		    		marginLeft: 76},
+		    		1000
+		    	);*/
+		    	$('.outer').animate({
+		    		width: 385},
+		    		1000
+		    	);
+		    	$('.section5 .centerHeading').animate({
+		    		fontSize: 28},
+		    		1000
+		    	);
+		    	$('.inner, .buttons').fadeOut(500);
+	    	});
+
+          	$('a.close').click(function(e) {
+                var whichWrap = $(this).parents(".activeSide");
+				e.stopPropagation();
+				e.preventDefault();
+	    		/* Act on the event */
+		    	$(whichWrap).animate({
+		    		right: -wrapperWidht
+		    	},1000);
+                $(whichWrap).removeClass("activeSide");
+		    	gh.animate({
+		    		left: 0},
+		    		1000);
+		    	/*$('.codersLogo').animate({
+		    		marginLeft: 'auto'},
+		    		1000
+		    	);*/
+		    	$('.outer').animate({
+		    		width: windowWidth},
+		    		1000
+		    	);
+		    	$('.section5 .centerHeading').animate({
+		    		fontSize: 48},
+		    		1000
+		    	);
+		    	$('.inner, .buttons').fadeIn(1500);
+	    	});
+
+			/*
+		    * Internal use of the demo website
+		    */
+		    /*
+		    $('#showExamples').click(function(e){
+				e.stopPropagation();
+				e.preventDefault();
+				$('#examplesList').toggle();
+			});
+
+			$('html').click(function(){
+				$('#examplesList').hide();
+			});*/
+
+
+            var slideshows = $('.cycle-slideshow').on('cycle-next cycle-prev', function(e, opts) {
+                // advance the other slideshow
+                slideshows.not(this).cycle('goto', opts.currSlide);
+            });
+
+            $('#cycle-2 .cycle-slide').click(function(){
+                var index = $('#cycle-2').data('cycle.API').getSlideIndex(this);
+                slideshows.cycle('goto', index);
+            });
         });
-    },2000);
-//});
-$(window).resize(function(){
-    console.log('hi');
-windowHeight = $(window).height();
-windowWidth = $(window).width();
-bgImg = $('.mainBg');
-bgImgHeight = windowHeight;
-bgImgWidth = (bgImgHeight/800)*780;
-bgImgLeft = (windowWidth-bgImgWidth)/2;
-bgImgLeftAnimate = bgImgLeft-(bgImgWidth/2.5);
-    bgImg.css({
-        height: bgImgHeight,
-        left:bgImgLeftAnimate
-    });
-
-    $('.home, .gallery, .registerForm').css({
-        minHeight:windowHeight
-    });    
-});
-$('a.rBtn').click(function(){
-    $.scrollTo($('#registerForm'),{top:'10px', left:'290px', duration: 2000} );
-    // $.scrollTo('#registerForm', 2500, {easing:'elasout'});
-})
-$('.home, .gallery, .registerForm').css({
-    minHeight:windowHeight
-});
-$('.homeInner, .galleryInner, .registerForm').css({
-//    display: 'none'
-    opacity:0
-})
-$(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    var homeHeight = $('.home').height();
-    var galleryHeight = $('.gallery').height();
-    var formShow = homeHeight + galleryHeight
-    //console.log(scrollTop);
-    if(scrollTop >= 100){
-        $('.galleryInner').stop().animate({
-            opacity:1
-        },1000);
-        $('.homeInner').stop().animate({
-            opacity:0
-        },1000);
-    }else{
-        //console.log('else');
-        $('.galleryInner').stop().animate({
-            opacity:0
-        },1000);
-        $('.homeInner').stop().animate({
-            opacity:1
-        },1000);
-    }
-});
-});
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,12 +2,12 @@ class AdminMailer < ActionMailer::Base
   default from: "vivek@webonise.com"
 
   def send_invite (email)
-    mail(:to =>email, subject: "Webonise Lab : Geek Hours 2014!")
+    mail(:to =>email, subject: "Webonise Lab : Geek Hours 2015!")
   end
 
   def send_hall_ticket(user, secondary_users)
     @user = user
     @secondary_users = secondary_users
-    mail(:to => user.email, subject: "Johnnie Coder: Hall Ticket of Geek Hours 2014!")
+    mail(:to => user.email, subject: "Johnnie Coder: Hall Ticket of Geek Hours 2015!")
   end
 end
