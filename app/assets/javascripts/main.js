e6b8494988d3b88556eb0441ff8eb0003482a465
Swapnil Vathare <swapnil@weboniselab.com>
2015-10-09 15:04:23 +0530

    landscape view done

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -33,6 +33,25 @@
 		    	$.fn.pagepiling.moveSectionDown();
 			});
 	    	var windowWidth = $(window).width();
+	    	var windowHegiht = $(window).height();
+	    	if(windowWidth<=windowHegiht){
+	    		$('body').addClass('portrait');
+	    		$('body').removeClass('landscape');
+	    	}else{
+	    		$('body').removeClass('portrait');
+	    		$('body').addClass('landscape');
+	    	}
+	    	$(window).resize(function(event) {
+		    	windowWidth = $(window).width();
+		    	windowHegiht = $(window).height();
+		    	if(windowWidth<=windowHegiht){
+		    		$('body').addClass('portrait');
+		    		$('body').removeClass('landscape');
+		    	}else{
+		    		$('body').removeClass('portrait');
+		    		$('body').addClass('landscape');
+		    	}
+	    	});
 	    	var wrapperWidht = windowWidth-385;
 	    	var registerWrap = $('.registerWrap');
             var galleryWrap = $(".galleryWrap");
